73e7a09e8e0e0d0bde1566f7bb6c308c6dc62ad9
bradmarr <bradmarranca@gmail.com>
2024-11-20 22:33:27 -0500

    COMPONENT LIKE HTML

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 using std::ifstream, std::ofstream;
 
 #include <iostream>
-using std::cerr, std::endl, std::cout, std::string;
+using std::cerr, std::endl, std::cout, std::string, std::isupper;
 
 const string PUBLIC_DIR = "public";
 const string OUT_DIR = "out";
@@ -18,18 +18,32 @@ int main() {
 
     char ch;
     while (in.get(ch)) {
-        if (ch == '|') {
-            std::string comp_dir = "";
-            while (in.get(ch) && ch != '|') {
-                comp_dir += ch;
-            }
-            ifstream comp(PUBLIC_DIR + "/" + comp_dir + ".html");
-            if (!comp) {
-                cerr << "Error opening file" << endl;
-                return 1;
-            }
-            while(comp.get(ch)) {
-                out << ch;
+        if (ch == '<') {
+            in.get(ch);
+            if (isupper(ch)) {
+                string comp_dir = string(1, ch);
+                while (in.get(ch) && ch != '/') {
+                    ch = ch == '-' ? '/' : ch;
+                    if (ch != ' ') {
+                        comp_dir += ch;
+                    }
+                }
+                in.get(ch);
+                
+                
+                ifstream comp(PUBLIC_DIR + "/" + comp_dir + ".html");
+                if (!comp) {
+
+                    cerr << "Error opening file `" << PUBLIC_DIR + "/" + comp_dir + ".html" << '`' << endl;
+                    return 1;
+                }
+
+                while(comp.get(ch)) {
+                    out << ch;
+                }
+            } else {
+                out << '<' << ch;
+                continue;
             }
         } else {
             out << ch;
